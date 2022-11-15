42031bed57ff32bdc12b55f6f5319feaeaf9c055
lzxmusta <1244453574@qq.com>
2022-11-15 11:45:34 +0800

    项目初始结构创建

---
+++ shoppingmall-coupon/src/test/java/com/lzxmusta/coupon/ShoppingmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzxmusta.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shoppingmall-member/src/test/java/com/lzxmusta/member/ShoppingmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzxmusta.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
